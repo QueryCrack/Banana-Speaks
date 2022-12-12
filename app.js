f4614c0567d7fbe220c2bdfbf6df57a2551b1395
QueryCrack <chaitanyahiremath007@gmail.com>
2022-12-12 15:39:35 +0530

    done

--- app.js
+++ app.js
@@ -2,11 +2,30 @@ var btnTranslate = document.querySelector("#btn-translate")
 var txtinput =  document.querySelector("#txt-area")
 var outputtxt = document.querySelector("#output")
 
+var serverURl = "https://api.funtranslations.com/translate/minion.json"
+
+function getTranslationURl (text){
+    return serverURl +"?" + "text=" + text
+}
+
+function errorhandler(error){
+    console.log("error occured" , error)
+    alert("something goes wrong!!")
+}
+
 console.log(txtinput)
+
 function clickHandler(){
     console.log("clicked")
-    outputtxt.innerHTML = txtinput.value
+    var inputText = txtinput.value
 
+    fetch(getTranslationURl(inputText))
+    .then(response => response.json())
+    .then(json=>{
+        var translatedText = json.contents.translated
+        outputtxt.innerHTML = translatedText
+    })
+    .catch(errorhandler)
     
 }
 
